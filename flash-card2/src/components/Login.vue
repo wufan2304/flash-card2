2c46f7db7c633c04173ceab3d674623914ad7ce0
wufan2304 <wufan-2304@foxmail.com>
2017-07-16 23:47:48 +0800

    增加了登陆界面，添加了后台代码

---
+++ flash-card2/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>{{msg}}</h1>
+    <input type="text" id="account">
+    <input type="text" id="password">
+    <button>login</button>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'this is login'
+    }
+  }
+}
+</script>
